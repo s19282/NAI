3508820f25fa83a7368a24b368082362d0fc6686
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-04-13 20:05:22 +0200

    nieskończona pętla

--- mpp3/src/Language.java
+++ mpp3/src/Language.java
@@ -1,19 +1,67 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Language
 {
     String name;
     List<String> trainingFiles = new ArrayList<>();
-    int[] vector = new int[26];
+    List<int[]> trainingVectors = new ArrayList<>();
+    double[] weights;
+    static final double alpha = 0.25;
+    static double threshold = 0.5;
 
     public void addTrainingFile(String trainingFile)
     {
         trainingFiles.add(trainingFile);
     }
+    public void addTrainingVector(String trainingFile)
+    {
+        int[] tmp = new int[26];
+        for(int i=0; i<trainingFile.length(); i++)
+            if (trainingFile.charAt(i)>=65&&trainingFile.charAt(i)<=90)
+                tmp[trainingFile.charAt(i)-65]++;
+        trainingVectors.add(tmp);
 
-    public Language(String name) {
+    }
+    public int getValue(Language language)
+    {
+        if(this.getName().equals(language.getName()))
+            return 1;
+        else
+            return 0;
+    }
+
+    public Language(String name)
+    {
         this.name = name;
+        weights=new Random().doubles(-5,5).limit(26).toArray();
+    }
+    public boolean learn(Language l,boolean stop)
+    {
+        for( int[] vector : trainingVectors)
+        {
+            System.out.println("vector");
+            double s;
+            while (true)
+            {
+                System.out.println("while");
+                s=0;
+                for (int i=0; i<vector.length; i++)
+                    s+=weights[i]*vector[i];
+
+                if(this.getValue(l)==1&&s<threshold||(this.getValue(l)==0&&s>=threshold))
+                {
+                    stop=true;
+                    for (int i=0; i<weights.length; i++)
+                        weights[i]=weights[i] + alpha*(this.getValue(l)-(s>=threshold?1:0))*(vector[i]);
+                }
+                else
+                    break;
+            }
+        }
+        return stop;
     }
 
     public String getName() {
@@ -32,11 +80,11 @@ public class Language
         this.trainingFiles = trainingFiles;
     }
 
-    public int[] getVector() {
-        return vector;
+    public double[] getWeights() {
+        return weights;
     }
 
-    public void setVector(int[] vector) {
-        this.vector = vector;
+    public void setWeights(double[] weights) {
+        this.weights = weights;
     }
 }
--- mpp3/src/Main.java
+++ mpp3/src/Main.java
@@ -3,31 +3,53 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 
-public class Main
-{
-    public static void main(String[] args) throws IOException
-    {
+public class Main {
+    public static void main(String[] args) throws IOException {
         List<Language> languages = new ArrayList<>();
         String path = "data";
         processDir(path, languages);
-        System.out.println(languages.toString());
+        learn(languages);
+    }
+
+    static void learn(List<Language> languages)
+    {
+        for (Language lang : languages)
+        {
+            System.out.println("lang");
+            boolean stop;
+            do
+            {
+                System.out.println("do");
+                stop = false;
+                for (Language language : languages)
+                {
+                    System.out.println("language");
+                    stop = lang.learn(language, stop);
+                }
+            }
+            while (stop);
+        }
+        System.out.println("Done");
     }
 
     static void processDir(String path, List<Language> languages) throws IOException
     {
         Files.walkFileTree(Paths.get(path), EnumSet.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE,
-                new SimpleFileVisitor<>() {
+                new SimpleFileVisitor<>()
+                {
                     Language tmp;
+
                     @Override
                     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
                     {
-                        if(dir.toString().equals(path))
+                        if (dir.toString().equals(path))
                             return CONTINUE;
                         else
                         {
@@ -40,25 +62,10 @@ public class Main
                     @Override
                     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
                     {
-                        tmp.addTrainingFile(Files.lines(file, StandardCharsets.UTF_8).collect(Collectors.joining(System.lineSeparator())));
+                        tmp.addTrainingFile(Files.lines(file, StandardCharsets.UTF_8).map(String::toUpperCase).collect(Collectors.joining(" ")));
+                        tmp.addTrainingVector(Files.lines(file, StandardCharsets.UTF_8).map(String::toUpperCase).collect(Collectors.joining(" ")));
                         return CONTINUE;
                     }
                 });
-
     }
-//    static void processDir(List<Language> languages)
-//    {
-//        try
-//        {
-//            for(Path path : Files.walk(Paths.get("data")).filter(path -> path.toString().endsWith(".txt")).collect(Collectors.toList()))
-//            {
-//
-//                System.out.println(path);
-//            }
-//        }
-//        catch (IOException ex)
-//        {
-//            ex.printStackTrace();
-//        }
-//    }
 }

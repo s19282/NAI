7d98201a2d65ae8b2089660e76c524ad925af9b3
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-05-18 16:31:34 +0200

    Wczytywanie danych z pliku

--- mpp6/src/Main.java
+++ mpp6/src/Main.java
@@ -1,6 +1,53 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException
+    {
+        int capacity = 0;
+        List<Set> sets = new ArrayList<>();
+        readFromFile(capacity,sets);
+
+        for(Set s : sets)
+            System.out.println(s.toString());
+
+    }
+
+    public static void  readFromFile(int capacity, List<Set> sets) throws IOException
+    {
+        Scanner s = new Scanner(Paths.get("plecak.txt"));
+        capacity = Integer.parseInt(s.nextLine().split(" ")[1]);
+        s.nextLine();
+        List<Integer> sizes = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+        Set tmpSet = new Set();
+        while (s.hasNext())
+        {
+            String line = s.nextLine();
+            Pattern p = Pattern.compile("(sizes|values)(\\d+): \\{((\\d+,)+\\d+)};");
+            Matcher m = p.matcher(line);
+            if(m.matches())
+            {
+                if(m.group(1).equals("sizes"))
+                {
+                    tmpSet.setNumber(Integer.parseInt(m.group(2)));
+                    Arrays.stream(m.group(3).split(",")).map(String::trim).mapToInt(Integer::parseInt).forEach(sizes::add);
+                    tmpSet.setSizes(sizes);
+                }
+                else if(m.group(1).equals("values"))
+                {
+                    Arrays.stream(m.group(3).split(",")).map(String::trim).mapToInt(Integer::parseInt).forEach(values::add);
+                    tmpSet.setValues(values);
+                    sets.add(tmpSet);
+                    tmpSet = new Set();
+                    sizes = new ArrayList<>();
+                    values = new ArrayList<>();
+                }
+            }
+        }
     }
 }
---
+++ mpp6/src/Set.java
@@ -0,0 +1,54 @@
+import java.util.List;
+
+public class Set implements Comparable<Set>
+{
+    int number;
+    List<Integer> sizes;
+    List<Integer> values;
+
+    public  Set(){}
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public List<Integer> getSizes() {
+        return sizes;
+    }
+    public int getSizes(int n) {
+        return sizes.get(n);
+    }
+
+    public void setSizes(List<Integer> sizes) {
+        this.sizes = sizes;
+    }
+
+    public List<Integer> getValues() {
+        return values;
+    }
+    public int getValue(int n) {
+        return values.get(n);
+    }
+
+    public void setValues(List<Integer> values) {
+        this.values = values;
+    }
+
+    @Override
+    public int compareTo(Set o) {
+        return this.number-o.number;
+    }
+
+    @Override
+    public String toString() {
+        return "Set{" +
+                "number=" + number +
+                ", sizes=" + sizes +
+                ", values=" + values +
+                '}';
+    }
+}
